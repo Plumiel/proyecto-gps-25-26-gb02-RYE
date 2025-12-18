73e54c8f9a66ff946b51012f19503196beb5c072
Driox07 <82775672+Driox07@users.noreply.github.com>
2025-12-18 16:56:18 +0100

    Docker preparaccción

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,7 +8,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Recomendaciones y Estadísticas (RYE)', 'host': '0.0.0.0'}, pythonic_params=True)
-    app.run(port=8084)
+    app.run(host=os.environ.get('HOST', '0.0.0.0'), port=int(os.environ.get('PORT', 8082)))
 
 
 if __name__ == '__main__':
